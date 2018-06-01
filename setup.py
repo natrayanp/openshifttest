7d46dcd3c65677c1676e3e194f285524118fee38
natrayanp <31285863+natrayanp@users.noreply.github.com>
2018-06-01 18:03:23 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='testapp',
       author='Natrayan',
       author_email='',
       url='http://www.python.org/sigs/distutils-sig/',
-     install_requires=['Flask>=0.7.2', 'MarkupSafe' , 'gunicorn>=0.19'],
+     install_requires=['Flask>=0.7.2', 'MarkupSafe','gunicorn'],
      )
